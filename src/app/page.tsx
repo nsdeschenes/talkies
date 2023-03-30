0fe065953015571708ec3281fab08dddaa563593
nsdeschenes <nsdeschenes@outlook.com>
2023-03-30 17:53:49 -0300

    add coming soon text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#E96479] to-[#4D455D]">
-      <h1 className="text-3xl font-semibold text-white">Talkies</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#E96479] to-[#4D455D] text-white">
+      <h1 className="text-3xl font-semibold ">Talkies</h1>
+      <p>Coming soon...</p>
     </main>
   );
 }
