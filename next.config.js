8560d010f952600d97e67dff630aa62e796116f2
nsdeschenes <nsdeschenes@outlook.com>
2023-04-09 12:31:33 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
+    // typedRoutes: true,
+    serverComponentsExternalPackages: ["@planetscale/database"],
+    turbo: true
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
